e541d8761708fe5546ebd99a415fcf1ba4c4e721
ravenwest94 <bladeshine19@gmail.com>
2016-11-28 15:18:47 +0300

    Add files via upload

---
+++ hotel/form.php
@@ -0,0 +1,18 @@
+﻿<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Форма</title>
+	</head>
+
+	<body>
+
+	<?php
+		echo "<p>Класс номера:" . $_POST["SelectClass"] . "</p>";
+		echo "<p>Фамилия: " . $_POST["FistName"] . "</p>";
+		echo "<p>Имя: " . $_POST["SecondName"] . "</p>";
+		echo "<p>Отчество: " . $_POST["ThirdName"] . "</p>";
+		echo "<p>Телефон: " . $_POST["phone_number"] . "</p>";	
+		echo "<p>Дата: " . $_POST["order_date"] . "</p>";
+	?>
+	</body>
+</html>
